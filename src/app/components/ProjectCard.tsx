ce5b29e1b47dcd3c2f6e9d4f7357001319db83b2
justindal <justindaludado@cmail.carleton.ca>
2024-07-29 06:35:38 -0400

    image optimization

--- src/app/components/ProjectCard.tsx
+++ src/app/components/ProjectCard.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Button from './Button'
+import Image from 'next/image'
 
 interface ProjectCardProps {
   title: string
   description: string
-  image?: string
+  image: string 
   children?: React.ReactNode
 }
 
@@ -18,7 +19,8 @@ const ProjectCard = ({
     <div className='hero bg-base-200 rounded-xl'>
       <div className='hero-content flex flex-col'>
         <h1 className='text-3xl font-bold'>{title}</h1>
-        <img src={image} className='max-w-sm rounded-lg shadow-2xl' />
+        {/* <img src={image} className='max-w-sm rounded-lg shadow-2xl' /> */}
+        <Image src={image} alt={title} width={500} height={500} className='max-w-sm rounded-lg shadow-2xl' />
         <p className='py-6'>{description}</p>
         {children}
       </div>
